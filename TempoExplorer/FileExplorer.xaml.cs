0593a805ba64a8f407d5ada94a3ab03ec0accc43
Zander Baker <zanderbaker100@gmail.com>
2021-08-10 14:44:34 -0700

    Initial commit for TempoExplorer

---
+++ TempoExplorer/FileExplorer.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TempoExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class FileExplorer
+    {
+        public FileExplorer()
+        {
+            InitializeComponent();
+        }
+    }
+}
