ebf5e9eab826775a0937c6c776f14afda337b6d8
Zander Baker <zanderbaker100@gmail.com>
2021-09-16 11:14:15 -0700

    Move StateMachine to Jammo.ParserTools

--- TempoPlugins/PluginParser.cs
+++ TempoPlugins/PluginParser.cs
@@ -126,46 +126,6 @@ namespace TempoPlugins
             return stream;
         }
 
-        private class StateMachine<TStates> : IEnumerable<TStates> where TStates : Enum
-        {
-            private readonly List<TStates> previous = new();
-
-            public TStates Current { get; private set; }
-
-            public StateMachine() { }
-            public StateMachine(TStates start)
-            {
-                Current = start;
-            }
-
-            public void MoveTo(TStates state)
-            {
-                previous.Add(Current);
-                Current = state;
-            }
-
-            public void MoveLast()
-            {
-                Current = previous.Last();
-                previous.Remove(Current);
-            }
-            
-            public TStates PeekLast()
-            {
-                return previous.Last();
-            }
-
-            public IEnumerator<TStates> GetEnumerator()
-            {
-                return previous.GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
-        }
-
         private enum ParserState
         {
             Any,
