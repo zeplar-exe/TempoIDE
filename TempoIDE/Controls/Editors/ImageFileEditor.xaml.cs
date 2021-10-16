9addca4ffe2169442cf12dc6b4ebf4d830c94a56
Zander Baker <zanderbaker100@gmail.com>
2021-10-15 20:33:22 -0700

    Add image drawing
    Add basic drawing to ImageFileEditor

--- TempoIDE/Controls/Editors/ImageFileEditor.xaml.cs
+++ TempoIDE/Controls/Editors/ImageFileEditor.xaml.cs
@@ -1,12 +1,20 @@
 using System;
 using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using TempoIDE.Core.Helpers;
 
 namespace TempoIDE.Controls.Editors
 {
     public partial class ImageFileEditor : FileEditor
     {
-        public override bool IsFocused { get; }
+        public bool Saved { get; private set; }
+        
+        public override bool IsFocused => Canvas.IsFocused;
         
         public ImageFileEditor()
         {
@@ -25,6 +33,32 @@ namespace TempoIDE.Controls.Editors
             return editor;
         }
         
+        private void Canvas_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Canvas.EraserShape = new EllipseStylusShape(15, 15);
+        }
+        
+        private void ImageFileEditor_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            return; // TODO: Figure out why erasing does not work
+            
+            if (DrawButton.IsChecked.GetValueOrDefault())
+            {
+                if (Canvas.EditingMode == InkCanvasEditingMode.EraseByPoint)
+                    Canvas.EditingMode = InkCanvasEditingMode.Ink;
+                else
+                    Canvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+            }
+        }
+        
+        private void DrawButton_OnIsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            if (Canvas.ActiveEditingMode == InkCanvasEditingMode.Ink)
+                Canvas.EditingMode = InkCanvasEditingMode.None;
+            else
+                Canvas.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
         public override void Refresh()
         {
             UpdateVisual();
@@ -32,7 +66,9 @@ namespace TempoIDE.Controls.Editors
 
         public override void Update(FileInfo file)
         {
+            BoundFile = file;
             
+            UpdateVisual();
         }
         
         public override void FileWriter()
@@ -50,7 +86,20 @@ namespace TempoIDE.Controls.Editors
 
         public override void UpdateFile()
         {
+            if (!Saved)
+                return;
             
-        }
+            var bitmap = new RenderTargetBitmap(
+                (int)Canvas.Width, (int)Canvas.Height, 
+                96d, 96d, PixelFormats.Default);
+            
+            bitmap.Render(Canvas);
+            
+            var encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+            
+            using var stream = BoundFile.OpenWrite();
+            encoder.Save(stream);
+        } // Courtesy of https://social.msdn.microsoft.com/Forums/vstudio/en-US/ba4dc89f-0169-43a9-8374-68e1fb34a222/saving-inkcanvas-as-image?forum=wpf
     }
 }
